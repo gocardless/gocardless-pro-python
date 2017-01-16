fc0be153c9347214856854a3c43631eeeb37510c
Crankshaft Robot <robot+crankshaft@gocardless.com>
2017-01-16 15:27:30 +0000

    Crankshaft[BU000003BRKWFC] Client - Python @ 2017-01-16 15:27:30 +0000
    1d5294f Tim Rogers <tim@gocardless.com>
    Merge pull request #16 from gocardless/gocardless-pro-python-pr-9
    
    Read the README file as UTF-8
    
    ----------------
    
    2a82c01 Tim Rogers <tim@gocardless.com>
    Read the README file as UTF-8
    
    https://github.com/gocardless/gocardless-pro-python/pull/9 attempts to fix an issue in our `setup.py` when trying to read `README.rst` which is UTF-8 encoded. It blows up by default, at least on some systems.
    
    The fix suggested in the PR didn't work, as the `encoding` argument to `open` doesn't seem to exist (see the Circle [build](https://circleci.com/gh/gocardless/crankshaft-test/4754)) but the wisdom of StackOverflow points to using this `codecs` library - see [here](http://stackoverflow.com/a/844443).
    
    This uses `codecs` for a UTF-8 safe way of reading the file.
    
    ----------------
    
    90be1d5 Tim Rogers <tim@gocardless.com>
    Merge pull request #15 from gocardless/revert-14-add-encoding-to-setup-file
    
    Revert "Fix for pound sign causing encode error in docker, python 3.4.3"
    
    ----------------
    
    9e62b0b Tim Rogers <tim@gocardless.com>
    Revert "Fix for pound sign causing encode error in docker, python 3.4.3"
    
    ----------------

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
+import codecs
 from setuptools import setup, find_packages
 
-with open('README.rst') as file:
+with codecs.open('README.rst', 'r', 'utf-8') as file:
     long_description = file.read()
 
 setup(
