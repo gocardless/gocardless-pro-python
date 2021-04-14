d076f60b68a528858729cedf5f176a5b917c43b5
Robot <robot@gocardless.com>
2021-04-14 12:55:03 +0000

    Changes generated by 194b4a48d7466a56c0bd750ce491bf7969944527

--- gocardless_pro/services/scenario_simulators_service.py
+++ gocardless_pro/services/scenario_simulators_service.py
@@ -47,6 +47,9 @@ class ScenarioSimulatorsService(base_service.BaseService):
 <li>`refund_bounced`: Transitions a refund to `bounced`. It must start in either the `pending_submission`, `submitted`, or `paid` state.</li>
 <li>`payout_bounced`: Transitions a payout to `bounced`. It must start in the `paid` state.</li>
 <li>`payout_create`: Creates a payout containing payments in `confirmed`, `failed` & `charged_back` states; refunds in `submitted` & `bounced`; and all related fees. Can only be used with a positive total payout balance and when some eligible items exist.</li>
+<li>`billing_request_fulfilled`: Authorises the billing request, fulfils it, and moves the associated payment to `failed`. The billing request must be in the `pending` state, with all actions completed except for `bank_authorisation`. Only billing requests with a `payment_request` are supported.<li>
+<li>`billing_request_fulfilled_and_payment_failed`: Authorises the billing request, fulfils it, and moves the associated payment to `failed`. The billing request must be in the `pending` state, with all actions completed except for `bank_authorisation`. Only billing requests with a `payment_request` are supported.<li>
+<li>`billing_request_fulfilled_and_payment_paid_out`: Authorises the billing request, fulfils it, and moves the associated payment to `paid_out`. The billing request must be in the `pending` state, with all actions completed except for `bank_authorisation`. Only billing requests with a `payment_request` are supported.<li>
 </ul>
               params (dict, optional): Request body.
 
