b7d01c584991cd45c9d6da3b47370acd7130eaf2
prolific117 <olu.oduro@gmail.com>
2022-12-30 09:16:45 +0000

    chore: fixed failing test

--- tests/integration/webhooks_integration_test.py
+++ tests/integration/webhooks_integration_test.py
@@ -8,6 +8,7 @@ import json
 import requests
 import responses
 from nose.tools import (
+  assert_in,
   assert_equal,
   assert_is_instance,
   assert_is_none,
@@ -29,6 +30,8 @@ def test_webhooks_list():
     helpers.stub_response(fixture)
     response = helpers.client.webhooks.list(*fixture['url_params'])
     body = fixture['body']['webhooks']
+
+    assert_in('?successful=true', responses.calls[-1].request.url)
 
     assert_is_instance(response, list_response.ListResponse)
     assert_is_instance(response.records[0], resources.Webhook)
