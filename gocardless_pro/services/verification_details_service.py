34ede99dd43999fb13864cc172203b2554058600
Robot <robot@gocardless.com>
2023-01-30 12:48:53 +0000

    Changes generated by 9d34fda261ce467ecd9ebc9b27c1a978ff8e32e7

--- gocardless_pro/services/verification_details_service.py
+++ gocardless_pro/services/verification_details_service.py
@@ -22,7 +22,7 @@ class VerificationDetailsService(base_service.BaseService):
 
         Verification details represent any information needed by GoCardless to
         verify a creditor.
-        Currently, only UK-based companies are supported. 
+        Currently, only UK-based companies are supported.
         In other words, to submit verification details for a creditor, their
         creditor_type must be company and their country_code must be GB.
 
